491e67c219765ff8b4ffa147255c0b79b146696c
develephant <code@develephant.com>
2017-07-11 17:14:24 -0500

    clean

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# coronium-chatterbox
-Lua based chat/messaging server.
+![logo](logo.png)
+
+# Coronium CS Installer
+
+___A Lua based real-time chat/messaging server.___
+
+__Status:__ _Early Beta_
+
+## Documentation
+
+[https://develephant.github.io/coronium-cs-docs](https://develephant.github.io/coronium-cs-docs)
+
+## Client Modules
+
+[Coronium CS Corona Client](https://github.com/develephant/coronium-cs-client)
---
+++ install/cookbooks/install/recipes/default.rb
@@ -0,0 +1,138 @@
+#
+# Cookbook Name:: cloud
+# Recipe:: cloud
+#
+# Copyright 2016-17 C. Byerley
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+apt_update 'update apt' do
+  action :update
+end
+
+apt_package [
+  'uuid',
+  'monit'
+]
+
+###############################################################################
+# User
+###############################################################################
+
+user 'coronium' do
+  manage_home true
+  home node['cs']['home']
+  shell '/bin/bash'
+  password node['cs']['password']
+end
+
+group 'sudo' do
+  action :modify
+  members ['coronium']
+end
+
+###############################################################################
+# Scaffold
+###############################################################################
+
+directory "#{node['cs']['home']}/logs" do
+  owner node['cs']['user']
+  group node['cs']['group']
+  mode '0755'
+  recursive true
+end
+
+file "#{node['cs']['home']}/logs/cs.log" do
+  content 'Log file...'
+  owner node['cs']['user']
+  group node['cs']['group']
+  mode '0660'
+end
+
+template "#{node['cs']['home']}/config.lua" do
+  source 'config.lua.erb'
+  owner node['cs']['user']
+  group node['cs']['group']
+  mode '0600'
+end
+
+cookbook_file "#{node['cs']['bin']}/chatterbox" do
+  source 'chatterbox'
+  owner node['cs']['user']
+  group node['cs']['group']
+  mode '0750'
+  action :create
+end
+
+template "#{node['cs']['monit']}/monitrc" do
+  source 'monitrc.erb'
+  mode '0600'
+end
+
+template "#{node['cs']['bin']}/cbrun" do
+  source 'bin/cbrun.erb'
+  owner node['cs']['user']
+  group node['cs']['group']
+  mode '0750'
+end
+
+###############################################################################
+# Logrotate
+###############################################################################
+file '/etc/logrotate.d/chatterbox' do
+  owner node['cs']['user']
+  group node['cs']['group']
+  mode '0664'
+  content <<-EOF
+#{node['cs']['home']}/logs/chatterbox.log {
+  size 3mb
+  copytruncate
+  create 0660 #{node['cs']['user']} #{node['cs']['group']}
+  dateext
+  rotate 1
+  compress
+}
+  EOF
+end
+
+###############################################################################
+# SSH
+###############################################################################
+bash 'allow passwords' do
+  cwd '/etc/ssh'
+  code <<-EOF
+  if [ -e sshd_config ]
+  then
+    sed 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' -i sshd_config
+  fi
+  EOF
+end
+
+service "ssh" do
+  action :reload
+end
+
+###############################################################################
+# Fin
+###############################################################################
+
+service "monit" do
+  action :reload
+end
+
+template "#{node['cs']['home']}/chatterbox.index" do
+  source 'chatterbox.index.erb'
+  owner node['cs']['user']
+  group node['cs']['group']
+  mode '0440'
+end
