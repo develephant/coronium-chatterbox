a9af4e300a8a79818d2c39eff8876564c79f6dcb
develephant <code@develephant.com>
2018-03-15 13:44:42 -0500

    build

--- install/cookbooks/install/recipes/monit.rb
+++ install/cookbooks/install/recipes/monit.rb
@@ -10,14 +10,14 @@ cookbook_file "#{node['cs']['monit']}/monitrc" do
   not_if "test -e #{node['cs']['monit']}/monitrc"
 end
 
-cookbook_file "#{node['cs']['monit']}/conf-available/monitrc_chatterbox" do
-  source 'monit/monitrc_chatterbox'
+cookbook_file "#{node['cs']['monit']}/conf-available/chatterbox_monit" do
+  source 'monit/chatterbox_monit'
   mode '0644'
 end
 
 # Link Enabled
-link "#{node['monit']['cs']}/conf-enabled/monitrc_chatterbox" do
-  to "#{node['monit']['cs']}/conf-available/monitrc_chatterbox"
+link "#{node['monit']['cs']}/conf-enabled/chatterbox_monit" do
+  to "#{node['monit']['cs']}/conf-available/chatterbox_monit"
 end
 
 service 'monit' do
